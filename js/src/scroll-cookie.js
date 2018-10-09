cd077c5c6851fc1a51dd2b5c21cc3719c2b58a89
oicu <oicu.cc@163.com>
2018-10-09 22:23:54 +0800

    Site updated: 2018-10-09 22:23:42

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Oct 09 2018 22:08:50 GMT+0800 (GMT+08:00)
+// build time:Tue Oct 09 2018 22:23:15 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
